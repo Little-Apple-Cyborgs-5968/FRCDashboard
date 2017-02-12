1987ab4d812aa82bd731ea6b812c9ead5d9cb65b
Stephanie Fu <stephaniefu@rocketmail.com>
2017-02-12 14:24:43 -0600

    Remove camera stream from dashboard

--- ui.js
+++ ui.js
@@ -217,19 +217,6 @@ function updatePressure(pressure) {
  $("#pressureNumber").html(pressure);
 }
 
-function cameraSelect() {
- $("#cameraSelection").css("visibility", "hidden");
- if ($("#camera8070").is(':checked')) {
-	 $("#camera1").css("visibility", "visible");
-	 $("#camera1").addClass("animated fadeInRight");
-	 $("#camera2").css("visibility", "hidden");
- } else if ($("#camera8080").is(':checked')) {
-	 $("#camera2").css("visibility", "visible");
-	 $("#camera2").addClass("animated fadeInRight");
-	 $("#camera1").css("visibility", "hidden");
- }
-}
-
 function changeClimb(color) {
   if (color === "green") {
     $("#climb").css({ fill: "#64ce74" });
@@ -364,18 +351,8 @@ function updateRobotPosition(dimension, pixel) {
   }
 }
 
-function switchCam(e) { // Switch camera feeds on key press
- // blue is 1, green is 2
- if (e.keyCode == 61) { // equals/plus button
-	 if ($("#camera1").css("visibility")==="visible") {
-		 $("#camera1").css("visibility", "hidden");
-		 $("#camera2").css("visibility", "visible");
-	 } else if ($("#camera2").css("visibility")==="visible") {
-		 $("#camera2").css("visibility", "hidden");
-		 $("#camera1").css("visibility", "visible");
-	 }
- }
- else if(e.keyCode == 99) {
+function keyPress(e) {
+ if(e.keyCode == 99) {
    console.log("Climbing now");
    changeClimb("green");
  } else if(e.keyCode == 115) {
@@ -504,7 +481,6 @@ function allianceSelect() {
     //NetworkTables.putNumber("/SmartDashboard/startPosition", 5);
   }
 
-	animate("#cameraSelection", "fadeInRight");
 	dashboardInit();
 }
 
